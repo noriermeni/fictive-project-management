782f590feecb5755d39481e389f2caff6ad91b25
noriermeni <nori.ermeni@gmail.com>
2022-02-12 00:13:28 +0100

    Test changes to deploy JSON Server in heroku

--- src/hook/useData.hook.ts
+++ src/hook/useData.hook.ts
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {URL_API} from "../utils/api";
 
 export default function useData(path: string) {
 
@@ -11,7 +12,7 @@ export default function useData(path: string) {
         (
             async function () {
                 try {
-                    const response = await axios.get(`http://localhost:5000/${path}`);
+                    const response = await axios.get(`${URL_API}${path}`);
                     setData(response.data);
                 } catch (err: any) {
                     setError(err);
---
+++ src/utils/api.ts
@@ -0,0 +1,1 @@
+export const URL_API = 'https://fictive-project-management.herokuapp.com/api';
